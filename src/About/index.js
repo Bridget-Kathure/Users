4491ee0446954252f020fa94e87e9f20ac37bef5
Bridget-Kathure <bridgetkathuretitus@gmail.com>
2024-06-20 15:34:34 +0300

    added files

---
+++ src/About/index.js
@@ -0,0 +1,11 @@
+
+import './index.css'
+export function About (){
+    return(
+        <div>
+        <h2>About Us</h2>
+        <p className="about-text">We are learning about ReactJs</p>
+        </div>
+        
+    )
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import Users from "./Users";
+import Login from "./Login";
+
+
+export const Introduction = () =>{
+  return(
+   <h1> We are AdaLab</h1>
+  )
+}
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Login/>
+      <Users/>
     </div>
   );
-}
+};
+
 
 export default App;
